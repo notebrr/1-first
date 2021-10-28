2ad06f06afaaf5f78acf54126cd1ee8d153f3237
notebrr <90389727+notebrr@users.noreply.github.com>
2021-10-28 11:04:52 +0200

    2pac

--- SP2/src/Garage.java
+++ SP2/src/Garage.java
@@ -2,56 +2,25 @@ import java.util.ArrayList;
 
 public class Garage {
     private ArrayList<Bil> cars = new ArrayList<>();;
-    private String carType;
-    private int regNr;
-    private String brandName;
-    private String model;
-    private int modelYear;
-    private int amountOfDoors;
 
     public Garage(){
 
     }
 
-    public void addCars(String carType, int regNr, String brandName, String model, int modelYear, int amountOfDoors){
-        this.carType = carType;
-        this.regNr = regNr;
-        this.brandName=brandName;
-        this.model = model;
-        this.modelYear = modelYear;
-        this.amountOfDoors = amountOfDoors;
-
-        Bil myCar = new Bil(regNr, brandName, model, modelYear, amountOfDoors) {
-            @Override
-            public double beregnGrønEjerafgift() {
-                return 0;
-            }
-        };
-
-        cars.add(myCar);
+    public void addCars(Bil bil){
+        cars.add(bil);
     }
 
     public double beregnGrønAfgiftForBilpar(){
         double sum = 0;
         for(Bil i : cars){
-            if (carType == "Benzinbil"){
-                Benzinbil benzinbil = new Benzinbil(i.getRegNr(), i.getBrandName(), i.getModel(), i.getModelYear(), i.getAmountOfDoors());
-                sum += benzinbil.beregnGrønEjerafgift();
-            }
-            if (carType == "Elbil"){
-                Elbil elbil = new Elbil(i.getRegNr(), i.getBrandName(), i.getModel(), i.getModelYear(), i.getAmountOfDoors());
-                sum += elbil.beregnGrønEjerafgift();
-            }
-            if (carType == "Dieselbil"){
-                Dieselbil dieselbil = new Dieselbil(i.getRegNr(), i.getBrandName(), i.getModel(), i.getModelYear(), i.getAmountOfDoors());
-                sum += dieselbil.beregnGrønEjerafgift();
-            }
+            sum = (sum + i.beregnGrønEjerafgift());
         }
         return sum;
     }
 
     @Override
     public String toString() {
-        return String.valueOf(cars);
+        return cars.toString();
     }
 }
--- SP2/src/Main.java
+++ SP2/src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Garage garage = new Garage();
-        garage.addCars("Elbil", 293975, "Tesla", "Model S", 2021, 5);
-        garage.addCars("Benzinbil", 293967, "Opel", "Asta", 2007, 4);
-        garage.addCars("Dieselbil", 393948, "Volkswagen", "Passat", 2010, 3);
+        Benzinbil opelAstra = new Benzinbil(293967, "Opel", "Asta", 2007, 4);
+        Elbil teslaModelS = new Elbil(293975, "Tesla", "Model S", 2021, 5);
+        Dieselbil volkswagenPassat = new Dieselbil(393948, "Volkswagen", "Passat", 2010, 3);
+
+
+        garage.addCars(opelAstra);
+        garage.addCars(teslaModelS);
+        garage.addCars(volkswagenPassat);
+
         System.out.println(garage + "\n");
         System.out.println("Samlet grøn ejerafgift: " + garage.beregnGrønAfgiftForBilpar());
     }
